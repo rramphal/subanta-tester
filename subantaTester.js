9866a1d0b08b254a3549d73b2e4e1af3ada92f8f
Ravi S. Rāmphal <rramphal@gmail.com>
2016-03-27 21:52:55 -0700

    added asterisks to duplicate forms

--- subantaTester.js
+++ subantaTester.js
@@ -189,9 +189,9 @@
 
     var FORMS = {
         sup  : _getSup(),
-        rāma : _splitForms('rāmaḥ rāmau rāmāḥ rāmam rāmau rāmān rāmeṇa rāmābhyāṃ rāmaiḥ rāmāya rāmebhyaḥ rāmāt rāmasya rāmayoḥ rāmāṇām rāme rāmeṣu'),
-        hari : _splitForms('hariḥ harī harayaḥ harim harī harīn hariṇā haribhyāṃ haribhiḥ haraye haribhyaḥ hareḥ hareḥ haryoḥ harīṇām harau hariṣu'),
-        pati : _splitForms('patiḥ patī patayaḥ patim patī patīn patyā patibhyāṃ patibhiḥ patye patibhyaḥ patyuḥ patyuḥ patyoḥ patīnām patyau patiṣu')
+        rāma : _splitForms('rāmaḥ rāmau* rāmāḥ rāmam rāmau* rāmān rāmeṇa rāmābhyāṃ rāmaiḥ rāmāya rāmebhyaḥ rāmāt rāmasya rāmayoḥ rāmāṇām rāme rāmeṣu'),
+        hari : _splitForms('hariḥ harī* harayaḥ harim harī* harīn hariṇā haribhyāṃ haribhiḥ haraye haribhyaḥ hareḥ* hareḥ* haryoḥ harīṇām harau hariṣu'),
+        pati : _splitForms('patiḥ patī* patayaḥ patim patī* patīn patyā patibhyāṃ patibhiḥ patye patibhyaḥ patyuḥ* patyuḥ* patyoḥ patīnām patyau patiṣu')
     };
 
     var OPTIONS = {
